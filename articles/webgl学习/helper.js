0f28f1dbda8840f680d4a9fe9d971f9219a1ec77
李茂生 <wb-lms629789@alibaba-inc.com>
2020-07-10 14:09:13 +0800

    完成球体绘制

--- articles/webgl学习/helper.js
+++ articles/webgl学习/helper.js
@@ -200,3 +200,523 @@
 
     return target;
   }
+
+  /**
+   *视图矩阵
+   *
+   * @param {*} cameraPosition，摄像机坐标
+   * @param {*} target，观察点坐标
+   * @param {*} upDirection，初始Y轴基向量
+   * @param {*} target
+   * @returns
+   */
+  function lookAt(cameraPosition, lookTarget, upDirection, target) {
+    var target = target || new Float32Array(16);
+    let zAxis = Vector3.normalize(
+      Vector3.subtractVectors(cameraPosition, lookTarget)
+    );
+    // 如果摄像机位置和目标位置处于同一点
+    if (zAxis.lengthSquare() === 0) {
+      zAxis.z = 1;
+    }
+
+    let xAxis = Vector3.normalize(Vector3.cross(upDirection, zAxis));
+    if (xAxis.length() == 0) {
+      if (Math.abs(upDirection.z == 1)) {
+        zAxis.x += 0.0001;
+      } else {
+        zAxis.z += 0.0001;
+      }
+      zAxis.normalize();
+      xAxis = Vector3.cross(upDirection, zAxis);
+      xAxis.normalize();
+    }
+    let yAxis = Vector3.normalize(Vector3.cross(zAxis, xAxis));
+
+    //第一列
+    target[0] = xAxis.x;
+    target[1] = xAxis.y;
+    target[2] = xAxis.z;
+    target[3] = 0;
+    //第二列
+    target[4] = yAxis.x;
+    target[5] = yAxis.y;
+    target[6] = yAxis.z;
+    target[7] = 0;
+    //第三列
+    target[8] = zAxis.x;
+    target[9] = zAxis.y;
+    target[10] = zAxis.z;
+    target[11] = 0;
+    //第四列
+    target[12] = cameraPosition.x;
+    target[13] = cameraPosition.y;
+    target[14] = cameraPosition.z;
+    target[15] = 1;
+
+    return target;
+  }
+
+  /**
+   *逆矩阵
+   *
+   * @param {*} m
+   * @param {*} target
+   * @returns
+   */
+  function inverse(m, target) {
+    var n11 = m[0],
+      n21 = m[1],
+      n31 = m[2],
+      n41 = m[3],
+      n12 = m[4],
+      n22 = m[5],
+      n32 = m[6],
+      n42 = m[7],
+      n13 = m[8],
+      n23 = m[9],
+      n33 = m[10],
+      n43 = m[11],
+      n14 = m[12],
+      n24 = m[13],
+      n34 = m[14],
+      n44 = m[15],
+      t11 =
+        n23 * n34 * n42 -
+        n24 * n33 * n42 +
+        n24 * n32 * n43 -
+        n22 * n34 * n43 -
+        n23 * n32 * n44 +
+        n22 * n33 * n44,
+      t12 =
+        n14 * n33 * n42 -
+        n13 * n34 * n42 -
+        n14 * n32 * n43 +
+        n12 * n34 * n43 +
+        n13 * n32 * n44 -
+        n12 * n33 * n44,
+      t13 =
+        n13 * n24 * n42 -
+        n14 * n23 * n42 +
+        n14 * n22 * n43 -
+        n12 * n24 * n43 -
+        n13 * n22 * n44 +
+        n12 * n23 * n44,
+      t14 =
+        n14 * n23 * n32 -
+        n13 * n24 * n32 -
+        n14 * n22 * n33 +
+        n12 * n24 * n33 +
+        n13 * n22 * n34 -
+        n12 * n23 * n34;
+
+    var determinant = n11 * t11 + n21 * t12 + n31 * t13 + n41 * t14;
+
+    if (determinant === 0) {
+      var msg = "inverse() can't invert m, determinant is 0";
+      console.warn(msg);
+      return identity();
+    }
+    target = target || new Float32Array(16);
+    var detInv = 1 / determinant;
+
+    target[0] = t11 * detInv;
+    target[1] =
+      (n24 * n33 * n41 -
+        n23 * n34 * n41 -
+        n24 * n31 * n43 +
+        n21 * n34 * n43 +
+        n23 * n31 * n44 -
+        n21 * n33 * n44) *
+      detInv;
+    target[2] =
+      (n22 * n34 * n41 -
+        n24 * n32 * n41 +
+        n24 * n31 * n42 -
+        n21 * n34 * n42 -
+        n22 * n31 * n44 +
+        n21 * n32 * n44) *
+      detInv;
+    target[3] =
+      (n23 * n32 * n41 -
+        n22 * n33 * n41 -
+        n23 * n31 * n42 +
+        n21 * n33 * n42 +
+        n22 * n31 * n43 -
+        n21 * n32 * n43) *
+      detInv;
+
+    target[4] = t12 * detInv;
+    target[5] =
+      (n13 * n34 * n41 -
+        n14 * n33 * n41 +
+        n14 * n31 * n43 -
+        n11 * n34 * n43 -
+        n13 * n31 * n44 +
+        n11 * n33 * n44) *
+      detInv;
+    target[6] =
+      (n14 * n32 * n41 -
+        n12 * n34 * n41 -
+        n14 * n31 * n42 +
+        n11 * n34 * n42 +
+        n12 * n31 * n44 -
+        n11 * n32 * n44) *
+      detInv;
+    target[7] =
+      (n12 * n33 * n41 -
+        n13 * n32 * n41 +
+        n13 * n31 * n42 -
+        n11 * n33 * n42 -
+        n12 * n31 * n43 +
+        n11 * n32 * n43) *
+      detInv;
+
+    target[8] = t13 * detInv;
+    target[9] =
+      (n14 * n23 * n41 -
+        n13 * n24 * n41 -
+        n14 * n21 * n43 +
+        n11 * n24 * n43 +
+        n13 * n21 * n44 -
+        n11 * n23 * n44) *
+      detInv;
+    target[10] =
+      (n12 * n24 * n41 -
+        n14 * n22 * n41 +
+        n14 * n21 * n42 -
+        n11 * n24 * n42 -
+        n12 * n21 * n44 +
+        n11 * n22 * n44) *
+      detInv;
+    target[11] =
+      (n13 * n22 * n41 -
+        n12 * n23 * n41 -
+        n13 * n21 * n42 +
+        n11 * n23 * n42 +
+        n12 * n21 * n43 -
+        n11 * n22 * n43) *
+      detInv;
+
+    target[12] = t14 * detInv;
+    target[13] =
+      (n13 * n24 * n31 -
+        n14 * n23 * n31 +
+        n14 * n21 * n33 -
+        n11 * n24 * n33 -
+        n13 * n21 * n34 +
+        n11 * n23 * n34) *
+      detInv;
+    target[14] =
+      (n14 * n22 * n31 -
+        n12 * n24 * n31 -
+        n14 * n21 * n32 +
+        n11 * n24 * n32 +
+        n12 * n21 * n34 -
+        n11 * n22 * n34) *
+      detInv;
+    target[15] =
+      (n12 * n23 * n31 -
+        n13 * n22 * n31 +
+        n13 * n21 * n32 -
+        n11 * n23 * n32 -
+        n12 * n21 * n33 +
+        n11 * n22 * n33) *
+      detInv;
+
+    return target;
+  }
+
+  /**
+   *根据视角求透视投影矩阵
+   *
+   * @param {*} viewRadians
+   * @param {*} aspect
+   * @param {*} near
+   * @param {*} far
+   * @param {*} target
+   * @returns
+   */
+  function perspective(viewRadians, aspect, near, far, target) {
+    //投影盒上边坐标
+    var top = near * Math.tan(Math.PI / 180) * 0.5 * viewRadians;
+    //投影盒高度
+    var height = 2 * top;
+    //投影盒宽度
+    var width = aspect * height;
+    //投影盒左边界坐标
+    var left = -0.5 * width;
+    return perspectiveOfRect(
+      left,
+      left + width,
+      top,
+      top - height,
+      near,
+      far,
+      target
+    );
+  }
+
+  /**
+   *求透视投影矩阵
+   *
+   * @param {*} left
+   * @param {*} right
+   * @param {*} top
+   * @param {*} bottom
+   * @param {*} near
+   * @param {*} far
+   * @param {*} target
+   * @returns
+   */
+  function perspectiveOfRect(left, right, top, bottom, near, far, target) {
+    target = target || new Float32Array(16);
+    var x = (2 * near) / (right - left);
+    var y = (2 * near) / (top - bottom);
+    var a = (right + left) / (right - left);
+    var b = (top + bottom) / (top - bottom);
+    var c = -(far + near) / (far - near);
+    var d = (-2 * far * near) / (far - near);
+
+    target[0] = x;
+    target[1] = 0;
+    target[2] = 0;
+    target[3] = 0;
+
+    target[4] = 0;
+    target[5] = y;
+    target[6] = 0;
+    target[3] = 0;
+
+    target[8] = a;
+    target[9] = b;
+    target[10] = c;
+    target[11] = -1;
+
+    target[12] = 0;
+    target[13] = 0;
+    target[14] = d;
+    target[15] = 0;
+
+    return target;
+  }
+
+  /*3维向量构造函数
+ *
+ * @param {*} x
+ * @param {*} y
+ * @param {*} z
+ */
+function Vector3(x, y, z) {
+  this.x = x || 0;
+  this.y = y || 0;
+  this.z = z || 0;
+}
+
+/**
+ *设置第一个分量
+ *
+ * @param {*} x
+ */
+
+Vector3.prototype.setX = function(x) {
+  this.x = x || 0;
+  return this;
+};
+
+/**
+ *设置第二个分量
+ *
+ * @param {*} y
+ */
+Vector3.prototype.setY = function(y) {
+  this.y = y || 0;
+  return this;
+};
+
+/**
+ *设置第三个分量
+ *
+ * @param {*} z
+ */
+Vector3.prototype.setZ = function(z) {
+  this.z = z || 0;
+  return this;
+};
+
+/**
+ *设置三个分量
+ *
+ * @param {*} x
+ * @param {*} y
+ * @param {*} z
+ */
+Vector3.prototype.set = function(x, y, z) {
+  this.x = x || 0;
+  this.y = y || 0;
+  this.z = z || 0;
+};
+
+/**
+ * 归一化向量
+ *
+ * @param {*} vec
+ */
+Vector3.prototype.normalize = function(vec) {
+  var length = this.length();
+
+  if (length > 0.00001) {
+    this.x /= length;
+    this.y /= length;
+    this.z /= length;
+  } else {
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+  }
+  return this;
+};
+
+/**
+ *向量加法
+ *
+ * @param {*} vec1
+ * @param {*} vec2
+ */
+Vector3.prototype.addVector = function(vec1, vec2) {
+  this.x = vec1.x + vec2.x;
+  this.y = vec1.y + vec2.y;
+  this.z = vec1.z + vec2.z;
+  return this;
+};
+
+/**
+ *向量长度
+ *
+ * @param {*} vec
+ */
+Vector3.prototype.length = function(vec) {
+  if (vec) {
+    return vec.length();
+  }
+  return Math.sqrt(this.lengthSquare());
+};
+
+/**
+ *向量长度的平方
+ *
+ * @param {*} vec
+ */
+Vector3.prototype.lengthSquare = function(vec) {
+  if (vec) {
+    return vec.lengthSquare();
+  }
+  return this.x * this.x + this.y * this.y + this.z * this.z;
+};
+
+/**
+ *向量加法
+ *
+ * @param {*} vec1
+ * @param {*} vec2
+ */
+Vector3.prototype.add = function(vec1, vec2) {
+  if (vec2) {
+    return this.addVector(vec1, vec2);
+  }
+  this.x += vec1.x;
+  this.y += vec1.y;
+  this.z += vec1.z;
+  return this;
+};
+
+/**
+ *向量减法，实例方法
+ *
+ * @param {*} vec1
+ * @param {*} vec2
+ */
+Vector3.prototype.sub = function(vec1, vec2) {
+  if (vec2) {
+    return this.addVector(vec1, -vec2);
+  }
+  this.x -= vec1.x;
+  this.y -= vec1.y;
+  this.z -= vec1.z;
+  return this;
+};
+
+/**
+ *向量减法，类方法
+ *
+ * @param {*} vec1
+ * @param {*} vec2
+ */
+Vector3.subtractVectors = function(vec1, vec2) {
+  return new Vector3(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z);
+};
+
+/**
+ *向量逐分量相乘
+ *
+ * @param {*} vec1
+ * @param {*} vec2
+ */
+Vector3.prototype.multiplyVectors = function(vec1, vec2) {
+  this.x = vec1.x * vec2.x;
+  this.y = vec1.y * vec2.y;
+  this.z = vec1.z * vec2.z;
+  return this;
+};
+
+/**
+ *向量逐分量相乘
+ *
+ * @param {*} vec1
+ * @param {*} vec2
+ */
+Vector3.prototype.multiply = function(vec1, vec2) {
+  if (vec2) {
+    return this.multiplyVectors(vec1, vec2);
+  }
+  this.x *= vec1.x;
+  this.y *= vec1.y;
+  this.z *= vec1.z;
+  return this;
+};
+
+/**
+ *向量点积
+ *
+ * @param {*} vec1
+ * @param {*} vec2
+ */
+Vector3.dot = function(vec1, vec2) {
+  return vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z;
+};
+
+/**
+ *向量差积
+ *
+ * @param {*} vec1
+ * @param {*} vec2
+ */
+Vector3.cross = function(vec1, vec2) {
+  var x = vec1.y * vec2.z - vec2.y * vec1.z;
+  var y = vec2.x * vec1.z - vec1.x * vec2.z;
+  var z = vec1.x * vec2.y - vec1.y * vec2.x;
+  return new Vector3(x, y, z);
+};
+
+/**
+ * 归一化向量
+ *
+ * @param {*} vec
+ */
+Vector3.normalize = function(vec) {
+  var length = vec.length();
+  if (length > 0.00001) {
+    return new Vector3(vec.x / length, vec.y / length, vec.z / length);
+  }
+  return new Vector3();
+};
