c4b827e5b29688fa429de015362b6d79746ee4aa
李茂生 <wb-lms629789@alibaba-inc.com>
2020-08-07 16:45:18 +0800

    fix js error.

--- demos/pwa2/js/index.js
+++ demos/pwa2/js/index.js
@@ -1,5 +1,7 @@
 var divs = document.querySelectorAll('.bottom div');
 var content = document.querySelector('.content');
-Array.prototype.forEach.call(divs, function(_, index) {
-  content.class = 'content color-' + (index + 1);
+Array.prototype.forEach.call(divs, function(div, index) {
+  div.onclick = function() {
+    content.className = 'content color-' + (index + 1);
+  }
 });
